8c495e085278ec7f1152d8f39c751fb7a9e2b991
Cassiano Ramos <cassianolori5@gmail.com>
2025-03-27 09:52:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-let texto=window.prompt("Digite seu nome: ")
-document.getElementById("titulo").innerHTML= "meu nome é " +texto;
+let contador = 0;
+const valor = document.getElementById("valor");
+const botaoMais = document.getElementById("mais");
+const botaoMenos = document.getElementById("menos");
+
+botaoMais.addEventListener("click", () => {
+    contador++;
+    atualizarContador();
+});
+
+botaoMenos.addEventListener("click", () => {
+    if (contador > 0) {
+        contador--;
+        atualizarContador();
+    }
+});
+
+function atualizarContador() {
+    valor.textContent = contador;
+    botaoMenos.disabled = contador === 0;
+}
+
+atualizarContador();
+
+
